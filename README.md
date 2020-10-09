123e13f25dedef7db95637c9c4c34070cfc2b382
Saksham Sharma <saksham@go-gaga-app.com>
2020-10-09 08:00:31 +0530

    bug fixes

--- README.md
+++ README.md
@@ -4,7 +4,18 @@ a python program that automatically helps automatically connect to a MS Teams me
 all you have to do is add a csv file which contains your time table information and change the location to the MS Teams app as per your computer storage
 keep following things in mind
 1) column 'Class_Name' contains the name of your Team in MS Teams
-2) coulmn 'Time' should be filled in the similar way as in the timeTable.csv file , i.e., HH:MM AM/PM-HH:MM AM/PM
-3) MS Teams should run in full screen (maximised form) 
+2) column 'Time' should be filled in the similar way as in the timeTable.csv file , i.e., HH:MM AM/PM-HH:MM AM/PM
+3) MS Teams should run in full screen (maximised form)
+
+Note:
+This program works on certain assumptions and only under those it will work properly
+1) classes should be pre-scheduled
+2) no reaction of any kind on any class should be there
+3) class should be scheduled on daily basis
+
+Having said this, you have the option to manipulate the code as per your situation .
+All you would need to change is the number of times 'tab' is pressed
+
+pyautogui source : https://github.com/asweigart/pyautogui
 
 feel free to contribute
--- manager.py
+++ manager.py
@@ -5,9 +5,11 @@ import pandas as pd
 from pandas import DataFrame
 from datetime import datetime, timedelta
 
+pg.FAILSAFE = False
+
 filePath = 'open /Applications/Microsoft\ Teams.app'
-openingTime = 30   #seconds(max time)
-timeTable = pd.read_csv('timeTable.csv')
+openingTime = 20   #seconds(max time)
+timeTable = pd.read_csv('test.csv')
 
 right = pg.size().width
 bottom = pg.size().height
@@ -17,43 +19,59 @@ indices = [day for day in timeTable['Day']]
 classes.index = indices
 classes.index.name = 'Day'
 
+today = datetime.today().strftime('%A')
 
 
 def openTeams():
-    today = datetime.today().strftime('%A')
-    timeStart = (datetime.today() +timedelta(minutes = 7)).strftime('%I:%M %p')
-    timeEnd = (datetime.now() + timedelta(hours = 1) +timedelta(minutes = 7)).strftime('%I:%M %p')
-    lectures = classes.loc[today]
-    currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
+    global today
 
-    if not currentLec.empty:
-        os.system(filePath)
-        time.sleep(openingTime)
-        pg.moveTo(right-80 , 16)
-        pg.click(right-80 , 10)
-        locateClass()
+    if not isClassOpen():
+        timeStart = (datetime.today() +timedelta(minutes = 3)).strftime('%I:%M %p')
+        timeEnd = (datetime.now() + timedelta(hours = 1) +timedelta(minutes = 3)).strftime('%I:%M %p')
+        lectures = classes.loc[today]
+        currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
+
+        if not currentLec.empty:
+            os.system(filePath)
+            time.sleep(openingTime)
+            pg.moveTo(right-80 , 16)
+            pg.click(right-80 , 10)
+            locateClass()
+        else:
+            time.sleep(30)
+            pg.moveTo(20 , 16,duration = 1)
+            pg.moveTo(right-20 , bottom-16,duration = 1)
+            openTeams()
     else:
-        time.sleep(30)
-        pg.moveTo(20 , 16,duration = 1)
-        pg.moveTo(right-20 , bottom-16,duration = 1)
-        openTeams()
+        locateClass()
 
 
 
 def locateClass():
-    today = datetime.today().strftime('%A')
-    timeStart = (datetime.today() +timedelta(minutes = 5)).strftime('%I:%M %p')
-    timeEnd = (datetime.now() + timedelta(hours = 1) +timedelta(minutes = 5)).strftime('%I:%M %p')
-
-    lectures = classes.loc[today]
-    currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
-
-    if not currentLec.empty:
-        print(currentLec.loc[ today , 'Class_Name'])
-        openClass(currentLec.loc[ today , 'Class_Name'])
+    global today
+    if not isClassOpen():
+        timeStart = (datetime.today() +timedelta(minutes = 2)).strftime('%I:%M %p')
+        timeEnd = (datetime.now() + timedelta(hours = 1) +timedelta(minutes = 2)).strftime('%I:%M %p')
+        lectures = classes.loc[today]
+        currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
+        if not currentLec.empty:
+            print(currentLec.loc[ today , 'Class_Name'])
+            openClass(currentLec.loc[ today , 'Class_Name'])
+        else:
+            time.sleep(30)
+            locateClass()
     else:
-        time.sleep(30)
-        locateClass()
+        timeStart = (datetime.today()+timedelta(minutes = 1)).strftime('%I:%M %p')
+        timeEnd = (datetime.now() + timedelta(hours = 1)+timedelta(minutes = 1)).strftime('%I:%M %p')
+        lectures = classes.loc[today]
+        currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
+        if not currentLec.empty:
+            print(currentLec.loc[ today , 'Class_Name'])
+            disconnectClass()
+            openClass(currentLec.loc[ today , 'Class_Name'])
+        else:
+            time.sleep(30)
+            locateClass()
 
 
 
@@ -91,18 +109,31 @@ def locateMeeting():
 
 
 
+def isClassOpen():
+    minutes = datetime.now().minute
+    timeStart = (datetime.today() -timedelta(minutes = minutes)).strftime('%I:%M %p')
+    timeEnd = (datetime.today() +timedelta(hours = 1) - timedelta(minutes = minutes)).strftime('%I:%M %p')
+    lectures = classes.loc[today]
+    currentLec = lectures[lectures['Time']==(timeStart+'-'+timeEnd)]
+    if not currentLec.empty:
+        print(currentLec.loc[ today , 'Class_Name'] +' class is going on')
+    return not currentLec.empty
 
 
-today = datetime.today().strftime('%A')
-lectures = [i for i,day in enumerate(classes.loc[today])]
+
+
+lectures = classes.loc[today]
+
+
 
 def joinClass():
     global lectures
     time.sleep(10)
-    pg.hotkey('tab')
+    for i in range(10):
+        pg.hotkey('tab')
     time.sleep(0.25)
     pg.hotkey('return')
-    time.sleep(openingTime)
+    time.sleep(5)
 
     pg.hotkey('tab')
     pg.hotkey('tab')
@@ -115,9 +146,25 @@ def joinClass():
 
     time.sleep(0.25)
     pg.hotkey('enter')
-    lectures.pop()
+    lectures = lectures[1:]
+    time.sleep(3)
+    isClassOpen()
+
+
+
+def disconnectClass():
+    pg.moveTo(right-80 , 16)
+    pg.click(right-80 , 10)
+    for i in range(13):
+        pg.hotkey('tab')
+
+    time.sleep(0.25)
+    pg.hotkey('enter')
+    pg.moveTo(right-80 , 16)
+    pg.click(right-80 , 10)
 
 
 while(len(lectures)>0):
-    print("lectures left = " + str(len(lectures)))
+    print("lectures left = "+str(len(lectures)))
+    print(lectures)
     openTeams()
